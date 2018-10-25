85374e0b28c3d084a6ad727f56dfb35631946358
kellyfire <kellyfire611@gmail.com>
2018-10-25 19:15:26 +0700

    Moi thay doi ...

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Duong Nguyen Phu cuong
+                   sssssssssssssssssssssssssssssss
                 </div>
 
                 <div class="links">
