aca868d75297898ea7bd51ee6336e7d667afa301
kellyfire <kellyfire611@gmail.com>
2018-10-25 18:51:30 +0700

    Moi hieu chinh giao dien trang chu~~~

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Duong Nguyen Phu cuong
                 </div>
 
                 <div class="links">
